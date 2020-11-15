7baab747a4087769b5f976a7f4ac021d35d22b00
ReinaIshikawa <ishirei017@gmail.com>
2020-11-16 02:38:55 +0900

    add test scripts that needed for writing a paper

---
+++ p_AudioProcessing.py
@@ -0,0 +1,141 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import pandas as pd
+import os
+import numpy as np
+from tqdm import tqdm
+import scipy.io.wavfile
+import time
+
+"""
+$ python preprocessing_T2.py --root D:\codes\dataset
+"""
+
+class AudioProcessing():
+    def __init__(self,sample_rate,signal,frame_length_t=0.025,frame_stride_t=0.01,nfilt =64):
+
+        self.sample_rate=sample_rate
+        self.signal = signal
+        self.frame_length_t=frame_length_t
+        self.frame_stride_t=frame_stride_t
+        self.signal_length_t=float(signal.shape[0]/sample_rate)
+        self.frame_length=int(round(frame_length_t * sample_rate)) #number of samples
+        self.frame_step=int(round(frame_stride_t * sample_rate))
+        self.signal_length = signal.shape[0]
+        self.nfilt=nfilt
+        self.num_frames = int(np.ceil(float(np.abs(self.signal_length - self.frame_length)) / self.frame_step))
+        self.pad_signal_length=self.num_frames * self.frame_step + self.frame_length
+        self.NFFT=512
+
+    def calc_MFCC(self):
+        pre_emphasis=0.97
+        emphasized_signal=np.concatenate([self.signal[0,:].reshape([1,-1]),  self.signal[1:,:] - pre_emphasis * self.signal[:-1,:]], 0)
+        z = np.zeros([self.pad_signal_length - self.signal_length,8])
+        pad_signal = np.concatenate([emphasized_signal, z], 0)
+        indices = np.tile(np.arange(0, self.frame_length), (self.num_frames, 1)) + np.tile(np.arange(0, self.num_frames * self.frame_step, self.frame_step), (self.frame_length, 1)).T
+        frames = pad_signal[indices.astype(np.int32, copy=False)]
+        frames=frames*np.hamming(self.frame_length).reshape(1,-1,1)
+        frames=frames.transpose(0,2,1)
+        mag_frames = np.absolute(np.fft.rfft(frames,self.NFFT))
+        pow_frames = ((1.0 / self.NFFT) * ((mag_frames) ** 2))
+        filter_banks = np.dot(pow_frames, self.cal_fbank().T)
+        filter_banks = np.where(filter_banks == 0, np.finfo(float).eps, filter_banks)
+        filter_banks = 20 * np.log10(filter_banks)  # dB
+        filter_banks =filter_banks.transpose(0,2,1)
+
+        return filter_banks
+
+    def cal_fbank(self):
+
+        low_freq_mel = 0
+        high_freq_mel = (2595 * np.log10(1 + (self.sample_rate / 2) / 700))
+        mel_points = np.linspace(low_freq_mel, high_freq_mel, self.nfilt + 2)
+        hz_points = (700 * (10**(mel_points / 2595) - 1))
+        bin = np.floor((self.NFFT + 1) * hz_points / self.sample_rate)
+        fbank = np.zeros((self.nfilt, int(np.floor(self.NFFT / 2 + 1))))
+        for m in range(1, self.nfilt + 1):
+            f_m_minus = int(bin[m - 1])   # left
+            f_m = int(bin[m])             # center
+            f_m_plus = int(bin[m + 1])    # right
+
+            for k in range(f_m_minus, f_m):
+                fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+            for k in range(f_m, f_m_plus):
+                fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+        return fbank
+
+
+
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type = str, default='./data')
+    parser.add_argument('--ratio_step', type =float, default=0.25)
+    args = parser.parse_args()
+
+    start = time.time()
+
+
+    root_pth = args.root
+    df = pd.read_csv('annotations.csv', header = 0)
+    df_len=len(df)
+    os.makedirs(os.path.join(root_pth, 'audio'), exist_ok=True)
+    mfcc_path = (os.path.join(root_pth, 'audio', 'mfcc'))
+    os.makedirs(mfcc_path,exist_ok=True)
+    count = 0
+    folder_count = [0]*9
+    folder_count_detail = [[] for _ in range(9)]
+    save_size = 64
+
+    t1_gt_f = [[], [], [], [], [], [], [], [], []]
+    t2_gt_f = [[], [], [], [], [], [], [], [], []]
+
+    for fileidx in range(df_len):
+        file_name = df.iat[fileidx, 2]
+        folder_num = df.iat[fileidx, 0]
+        start_time =  df.iat[fileidx, 9]
+        end_time = df.iat[fileidx, 10]
+        filling_type = df.iat[fileidx, 4]
+        filling_level = df.iat[fileidx, 5]
+
+        fi_type = [0] * 4
+        fi_level = [0] * 3
+
+        t1_gt_f[int(folder_num)].append(filling_level)
+        t2_gt_f[int(folder_num)].append(filling_type)
+
+        audio_filename = file_name.rsplit("_", 1)[0] + '_audio.wav'
+        audio_path = os.path.join(root_pth, str(folder_num), 'audio', audio_filename)
+        sample_rate, signal = scipy.io.wavfile.read(audio_path)
+
+        ap = AudioProcessing(sample_rate,signal,nfilt=save_size)
+        mfcc = ap.calc_MFCC()
+        mfcc_length=mfcc.shape[0]
+        if mfcc_length < save_size:
+            print("file {} is too short".format(fileidx))
+        else:
+            f_step=int(mfcc.shape[1]*args.ratio_step)
+            f_length=mfcc.shape[1]
+            save_mfcc_num=int(np.ceil(float(np.abs(mfcc_length - save_size)) / f_step))
+            folder_count_detail[folder_num - 1].append(save_mfcc_num)
+            folder_count[folder_num - 1] += 1
+            fi_level[int(filling_level)] += 1
+            for i in range(save_mfcc_num):
+                pos= 0
+                if start_time/ap.signal_length_t*mfcc_length<i*f_step+f_length*0.75 and end_time/ap.signal_length_t*mfcc_length>i*f_step+f_length*0.25:
+                    pos = 1
+
+                fi_type[int(pos*filling_type)]+= 1
+        print('fi_type: ', fi_type, '\tfi_level: ', fi_level)
+
+    print('folder_count: ' folder_count)
+    print('folder_count_detail: ', folder_count_detail)
+    elapsed_time = time.time() - start
+    print("elapsed_time:{}".format(elapsed_time) + "sec")
+
+    np.save(os.path.join(root_pth, 'audio', 't1_gt'), np.array(t1_gt_f))
+    np.save(os.path.join(root_pth, 'audio', 't2_gt'), np.array(t2_gt_f))
---
+++ p_test.py
@@ -0,0 +1,215 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from model import Net, LSTMNet
+from AudioProcessing import AudioProcessing
+from Mydataset import Padding
+from utils_t3 import *
+
+import numpy as np
+import scipy.io.wavfile
+import os
+from tqdm import tqdm
+import pandas as pd
+from glob import glob
+import csv
+import pdb
+import pickle
+
+import torch
+import time
+import random
+
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import f1_score
+
+"""
+$ python test.py --root D:\codes\dataset --train-type part --folder_num 3 --val 3
+"""
+
+def compute_WAFS(gt, est):
+	gt = gt.astype(str)
+	est = est.astype(str)
+	return f1_score(gt, est, average='weighted')
+
+def compute_precision_recall_F1(gt, est):
+	gt = gt.astype(str)
+	est = est.astype(str)
+	return precision_recall_fscore_support(gt, est, average='weighted')
+
+def compute_acc(gt, est):
+    gt = gt.astype(str)
+	est = est.astype(str)
+	return accuracy_score(gt, est)
+
+def load_checkpoint_T2(file_path, use_cuda=False):
+    checkpoint = torch.load(file_path) if use_cuda else \
+        torch.load(file_path, map_location=lambda storage, location: storage)
+    model = Net()
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
+
+def load_checkpoint_T1(file_path, use_cuda=False):
+    checkpoint = torch.load(file_path) if use_cuda else \
+        torch.load(file_path, map_location=lambda storage, location: storage)
+    model = LSTMNet(is_cuda=use_cuda,
+                    input_dim=checkpoint['input_dim'],
+                    output_dim=checkpoint['class_num'],
+                    hidden_dim=checkpoint['hidden_dim'],
+                    n_layers=checkpoint['n_layers'],
+                    drop_prob=checkpoint['drop_prob'])
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
+
+
+"""
+Constant
+"""
+mfcc_MAX_VALUE=194.19187653405487
+mfcc_MIN_VALUE=-313.07119549054045
+
+t2_MAX_VALUE = 57.464638
+t2_MIN_VALUE = -1.1948369
+save_size=64
+
+"""Setup"""
+random.seed(0)
+np.random.seed(0)
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+if __name__ == "__main__":
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', action='store', type = str, default='./data')
+    parser.add_argument('--folder_num', action='store',type=int, nargs='*',default=[10, 11, 12],help='name of folder that you want to test')
+    parser.add_argument('--train_type', action='store', type = str, default='all',
+                        choices=['all', 'part'],
+                        help='please set the same string as you have put in training phase!! train with all train data or use part of train data as validation [default: all]')
+    parser.add_argument('--val', type=int, default=-1, metavar='N',
+                        choices=[-1,1,2,3,4,5,6,7,8,9],
+                        help = 'please set the same number as you have put in training phase!! which train folder is used as validation (not trained) [default: -1]')
+    parser.add_argument('--loss_type', action='store', type = str, default = 'CE',
+                        choices=['FL', 'CE'],
+                        help='please set the same string as you have put in training phase!! loss type (Focal loss/Cross entropy) [default: CE]')
+    parser.add_argument('--t1_epochs', type=int, default=400, metavar='N',
+                        help='please set the same number as you have put in training phase!! number of epochs to train [default:400]')
+    parser.add_argument('--t2_epochs', type=int, default=200, metavar='N',
+                        help='please set the same number as you have put in training phase!! number of epochs to train [default:200]')
+    parser.add_argument('--step_T3', type = int, default=8,help='step of frame for object detection using maskrcnn')
+    parser.add_argument('--view', action='store',type=str,default="c1",help='which view you use for T3')
+    args = parser.parse_args()
+
+
+    header = ['Container ID','Sequence', 'Container Capacity','Filling type', 'Filling level']
+    print('Container, ID Sequence, Container Capacity, Filling type, Filling level')
+
+    use_cuda = torch.cuda.is_available()
+    root_dir = args.root
+
+    model_dir = os.path.join(root_dir, './models')
+    model_T2=load_checkpoint_T2(os.path.join(model_dir, "T2_{}_{}_{}_{}.pth".format(args.train_type,args.val,args.loss_type,args.t2_epochs)), use_cuda=use_cuda)
+
+    model_T1 = load_checkpoint_T1(os.path.join(model_dir, "T1_{}_{}_{}_{}.pth".format(args.train_type, args.val, args.loss_type, args.t1_epochs)), use_cuda=use_cuda)
+
+    if use_cuda:
+        model_T2.cuda()
+        model_T1.cuda()
+
+    model_T2.eval()
+    model_T1.eval()
+    hidden = model_T1.init_hidden(1)
+    t2_answer_list = []
+    t1_answer_list = []
+    t2_gt = np.load(os.path.join(root_dir, 'audio', 't2_gt.npy'))
+    t1_gt = np.load(os.path.join(root_dir, 'audio', 't1_gt.npy'))
+
+    pad = Padding(100)
+    DT=Detection()
+    for folder_num in args.folder_num:
+        time_sum = 0
+        count = 0
+        pth = os.path.join(root_dir,str(folder_num), 'audio')
+        pth_rgb=os.path.join(root_dir,str(folder_num), 'rgb')
+        files = glob(pth + "/*")
+        #print("folder_num:{}".format(folder_num))
+
+        for file in sorted(files):
+            count+= 1
+            t12_start = time.time()
+
+            predlist = []
+            datalist = []
+            count_pred=[0,0,0,0]
+            seqence=int(file.split(os.path.sep)[-1].split("_")[0])
+            #seqence=-1
+            #print("file_num:{}".format(seqence), end='')
+            sample_rate, signal = scipy.io.wavfile.read(file)
+            ap = AudioProcessing(sample_rate,signal)
+            mfcc = ap.calc_MFCC()
+            mfcc_length=mfcc.shape[0]
+            f_step=int(mfcc.shape[1]*0.25)
+            f_length=mfcc.shape[1]
+            save_mfcc_num=int(np.ceil(float(np.abs(mfcc_length - save_size)) /f_step))
+            for i in range(save_mfcc_num):
+                tmp_mfcc = mfcc[i*f_step:save_size+i*f_step,: ,:]
+                tmp_mfcc= (tmp_mfcc-mfcc_MIN_VALUE)/(mfcc_MAX_VALUE-mfcc_MIN_VALUE)
+                tmp_mfcc=tmp_mfcc.transpose(2,0,1)
+                audio=torch.from_numpy(tmp_mfcc.astype(np.float32))
+                audio=torch.unsqueeze(audio, 0)
+                if use_cuda:
+                    audio = audio.cuda()
+                #Task 2
+                with torch.no_grad():
+                    mid, pred_T2=model_T2.before_lstm(audio)
+                    _,pred_T2=torch.max(pred_T2,1)
+                    count_pred[pred_T2.item()]+=1
+                    predlist.append(pred_T2.item())
+
+                    datalist.append(mid.to('cpu').detach().numpy().copy())
+
+            if  count_pred[1]>5 or count_pred[2]>5 or count_pred[3]>5:
+                final_pred_T2=count_pred[1:4].index(max(count_pred[1:4]))+1
+            else:
+                final_pred_T2=0
+            #print(" pred filling type: {}".format(final_pred_T2))
+
+            datalist = np.squeeze(np.array(datalist))
+            predlist = np.squeeze(np.array(predlist))
+
+            data = (datalist-t2_MIN_VALUE)/(t2_MAX_VALUE-t2_MIN_VALUE)
+            data = pad(data, predlist)
+            data = torch.from_numpy(data.astype(np.float32))
+            data = torch.unsqueeze(data, 0)
+            if use_cuda:
+                    data = data.cuda()
+            with torch.no_grad():
+                hidden = tuple([each.data for each in hidden])
+                outputs, hidden = model_T1(data, hidden)
+                _,pred_T1=torch.max(outputs,1)
+
+            t12_end = time.time()
+            time_sum += (t12_end - t12_start)
+            t2_answer_list.append(final_pred_T2)
+            t1_answer_list.append(pred_T1)
+
+        print('time_average:{}sec'.format(time_sum / count))
+        t2_ans = np.array(t2_answer_list)
+        t1_ans = np.array(t1_answer_list)
+
+        t2_acc = compute_acc(t2_gt, t2_ans)
+        t1_acc = compute_acc(t1_gt, t1_ans)
+
+        t2_pre, t2_recall, t2_f, _ = compute_precision_recall_F1(t2_gt, t2_ans)
+        t1_pre, t1_recall, t1_f, _ = compute_precision_recall_F1(t1_gt, t1_ans)
+
+        t2_wafs = compute_WAFS(t2_gt, t2_ans)
+        t1_wafs 0 compute_WAFS(t1_gt, t1_ans)
+
+        print('t2 --- acc:{}, precision:{}, recall:{}, f1:{}, wafs:{}'.format(t2_acc, t2_pre, t2_recall, t2_f, t2_wafs))
+        print('t1 --- acc:{}, precision:{}, recall:{}, f1:{}, wafs:{}'.format(t1_acc, t1_pre, t1_recall, t1_f, t1_wafs))
